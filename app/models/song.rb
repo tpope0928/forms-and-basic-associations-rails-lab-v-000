c0b3f38d5f40bcf5c52015de78c2000f2a1d65ed
Thomas Pope <tpope0928@gmail.com>
2019-08-11 21:31:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,7 +12,14 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
+  def genre_name=(genre)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
 
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+  
 
 
 end
