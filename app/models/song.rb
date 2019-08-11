3240552bff4e24944044ef2fb65d4dd3c89b1296
Thomas Pope <tpope0928@gmail.com>
2019-08-11 21:29:04 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,10 +4,15 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def genre_name=(name)
-    genre = Genre.find_or_create_by(name)
-    self.genre = genre
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  
 
 
 end
