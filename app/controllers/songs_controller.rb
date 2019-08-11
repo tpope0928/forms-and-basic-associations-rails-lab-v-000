3034d2b6238294e981426d27a890d26ebd405e67
Thomas Pope <tpope0928@gmail.com>
2019-08-11 21:25:13 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
     @song = Song.new(song_params)
 
     if @song.save
-      redirect_to @song
+      render :new
     else
       render :new
     end
